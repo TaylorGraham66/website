9fa04ce3d1af5eb20ad2ad291b4cef74d7e64c39
Taylor.Graham66 <tjkngraham@gmail.com>
2026-03-26 15:45:33 -0500

    Home update & added navbar w files

---
+++ app/experience/page.tsx
@@ -0,0 +1,3 @@
+export default function About() {
+  return <h1>Experience Page</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className = "main-div">
-          <div className = "main-desc">
-            <img className = "main-img" src="/images/IMG_7580.jpg" />
-          </div>  
-          <div className = "main-desc">
-              <h1>Taylor Graham Portfolio Website</h1>
-          </div>
+      <div className = "flex items-center justify-center h-screen gap-12 px-20">
+        <div className="max-w-lg">
+          <h1 className="text-4xl font-bold mb-4">Portfolio Website</h1>
+          <p className="text-lg">
+            Hello! My name is Taylor Graham and this is my portfolio website. This website displays my experience, skills, and projects.
+          </p>
+        </div>
+        <div>
+          <img className = "h-80 w-60 rounded-xl shadow-lg" src="/images/IMG_7580.jpg" />
+        </div>
       </div>
     </div>
   );
